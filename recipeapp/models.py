c874541855b50bea5d30589d627e8dc842bc9f1f
Cynthia Kitili <nyambucindy0@gmail.com>
2021-01-29 00:14:49 +0300

    commit models created

--- recipeapp/models.py
+++ recipeapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Chef(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+
+class food(models.Model):
+    name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class recipe(models.Model):
+    name = models.CharField(max_length =60)
+    ingredients = models.TextField()
+    procedure = models.TextField()
+    chef = models.ForeignKey(Chef, on_delete=models.CASCADE)
+    food=models.ManyToManyField(food)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    
--- recipeproject/settings.py
+++ recipeproject/settings.py
@@ -78,10 +78,13 @@ WSGI_APPLICATION = 'recipeproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'recipe',
+        'USER': 'cynthia',
+    'PASSWORD':'gitz254',
     }
 }
+
 
 
 # Password validation
@@ -108,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
