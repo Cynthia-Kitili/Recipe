7d3247f5c9d6e1948fac6a7326aa08d2c2fdb925
Cynthia Kitili <nyambucindy0@gmail.com>
2021-01-29 21:40:01 +0300

    add loop for recipes in today and past recipes

--- recipeapp/urls.py
+++ recipeapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns=[
     # url('^$',views.welcome,name = 'welcome'),
-    url('^$',views.recipe_of_day,name='recipeToday'),
+    url('^$',views.recipe_day,name='recipeToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_recipe,name = 'pastRecipe')
 
 ]
--- recipeapp/views.py
+++ recipeapp/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 import datetime as dt
-
+from .models import Recipe
 
 # Create your views here.
 def welcome(request):
     return render(request,'welcome.html')
 
-def recipe_of_day(request):
+def recipe_today(request):
     date = dt.date.today()
-    return render(request, 'all-recipes/today-recipe.html', {"date": date,})   
+    recipe = Recipe.todays_recipe()
+
+    return render(request, 'all-recipes/today-recipe.html', {"date": date,"recipe":recipe})   
 
 def past_days_recipe(request, past_date):
 
@@ -23,7 +25,8 @@ def past_days_recipe(request, past_date):
         assert False
 
     if date == dt.date.today():
-        return redirect(recipe_of_day)
+        return redirect(recipe_today)
+    recipe = Recipe.days_recipe(date)
 
-    return render(request, 'all-recipes/past-recipe.html', {"date": date})
+    return render(request, 'all-recipes/past-recipe.html', {"date": date,"recipe":recipe})
 
