11189605a83ac28868c8c9e40de0a75b7d03f23a
Cynthia Kitili <nyambucindy0@gmail.com>
2021-01-29 22:33:08 +0300

    add image display

--- recipeproject/settings.py
+++ recipeproject/settings.py
@@ -65,10 +65,12 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'recipeproject.wsgi.application'
 
@@ -128,4 +130,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
